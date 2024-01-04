d55f7ed319925d0b6e18713be08206a805104974
Silpashree Acharya <silpashree@bootlabstech.com>
2024-01-04 17:22:33 +0530

    cmek

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
 resource "aws_dynamodb_table" "dynamodb_table" {
-for_each           = { for dynamodb_table in var.dynamodb_table_details : dynamodb_table.dynamodb_table_name => dynamodb_table }
-  name             = each.value.dynamodb_table_name
-  hash_key         = each.value.hash_key
-  range_key        = each.value.range_key
-  stream_enabled   = each.value.stream_enabled
-  stream_view_type = each.value.stream_view_type
-  table_class      = each.value.table_class
+  name             = var.dynamodb_table_name
+  hash_key         = var.hash_key
+  range_key        = var.range_key
   billing_mode     = var.billing_mode
 
+  read_capacity    = var.read_capacity
+  write_capacity   = var.write_capacity
+  stream_enabled   = var.stream_enabled
+  stream_view_type = var.stream_view_type
+  table_class      = var.table_class
+  deletion_protection_enabled = var.deletion_protection_enabled
+
+
+
   dynamic "attribute" {
-    for_each = each.value.attributes
+    for_each = var.attributes
 
     content {
       name = attribute.value.name
@@ -18,13 +23,15 @@ for_each           = { for dynamodb_table in var.dynamodb_table_details : dynamo
   }
 
   ttl {
-    enabled        = each.value.ttl_enabled
-    attribute_name = each.value.ttl_attribute_name
+    enabled        = var.ttl_enabled
+    attribute_name = var.ttl_attribute_name
   }
 
   point_in_time_recovery {
-    enabled = each.value.point_in_time_recovery_enabled
+    enabled = var.point_in_time_recovery_enabled
   }
+
+
 
   dynamic "local_secondary_index" {
     for_each = var.local_secondary_indexes
@@ -57,8 +64,40 @@ for_each           = { for dynamodb_table in var.dynamodb_table_details : dynamo
     content {
       region_name = replica.value.region_name
       kms_key_arn = lookup(replica.value, "kms_key_arn", null)
+
+
     }
   }
 
+  server_side_encryption {
+    enabled     = var.server_side_encryption_enabled
+    kms_key_arn = var.server_side_encryption_kms_key_arn
+  }
 
+  lifecycle {
+     ignore_changes = [replica,read_capacity,write_capacity] 
+  }
 }
+# resource "aws_appautoscaling_target" "dynamodb_table_read_target" {
+#   max_capacity       = 100
+#   min_capacity       = 5
+#   resource_id        = aws_dynamodb_table.dynamodb_table.name
+#   scalable_dimension = "dynamodb:table:ReadCapacityUnits"
+#   service_namespace  = "dynamodb"
+# }
+
+# resource "aws_appautoscaling_policy" "dynamodb_table_read_policy" {
+#   name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.dynamodb_table_read_target.resource_id}"
+#   policy_type        = "TargetTrackingScaling"
+#   resource_id        = aws_appautoscaling_target.dynamodb_table_read_target.resource_id
+#   scalable_dimension = aws_appautoscaling_target.dynamodb_table_read_target.scalable_dimension
+#   service_namespace  = aws_appautoscaling_target.dynamodb_table_read_target.service_namespace
+
+#   target_tracking_scaling_policy_configuration {
+#     predefined_metric_specification {
+#       predefined_metric_type = "DynamoDBReadCapacityUtilization"
+#     }
+
+#     target_value = 70
+#   }
+# }
--- variables.tf
+++ variables.tf
@@ -1,28 +1,41 @@
 variable "region" {
-  type    = string
+  type = string
 }
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table.This name is unique within a region."
+  type        = string
+}
+
 variable "billing_mode" {
-  description = "Controls how you are billed for read/write throughput and how you manage capacity. The valid values are PROVISIONED or PAY_PER_REQUEST"
+  description = "Controls how you are billed for read/write throughput and how you manage capacity."
   type        = string
 }
-variable "dynamodb_table_details" {
-  description = "dynamodb_table_details"
-  type        =  list(any)
-}
-/*variable "dynamodb_table_name" {
-  description = "Name of the DynamoDB table"
-  type        = string
-}
+
 variable "hash_key" {
-  description = "The attribute to use as the hash (partition) key. Must also be defined as an attribute"
+  description = "The attribute to used as the hash (partition) key. Must also be defined as an attribute"
   type        = string
 }
+
 variable "range_key" {
   description = "The attribute to use as the range (sort) key. Must also be defined as an attribute"
   type        = string
 }
+
+variable "read_capacity" {
+  description = "The number of read units for this table. If the billing_mode is PROVISIONED, this field should be greater than 0"
+  type        = number
+}
+
+variable "write_capacity" {
+  description = "The number of write units for this table. If the billing_mode is PROVISIONED, this field should be greater than 0"
+  type        = number
+}
 variable "stream_enabled" {
   description = "Indicates whether Streams are to be enabled (true) or disabled (false)."
+  type        = bool
+}
+variable "deletion_protection_enabled" {
+  description = "Indicates whether the DynamoDB table is protected against deletion or not."
   type        = bool
 }
 variable "table_class" {
@@ -33,51 +46,53 @@ variable "table_class" {
 variable "stream_view_type" {
   description = "(Optional) When an item in the table is modified, StreamViewType determines what information is written to the table's stream. Valid values are KEYS_ONLY, NEW_IMAGE, OLD_IMAGE, NEW_AND_OLD_IMAGES."
   type        = string
-  default     = null
 }
 
 variable "ttl_enabled" {
   description = "Indicates whether ttl is enabled"
   type        = bool
-  default     = false
 }
 
 variable "ttl_attribute_name" {
   description = "The name of the table attribute to store the TTL timestamp in"
   type        = string
-  default     = ""
 }
 
 variable "point_in_time_recovery_enabled" {
   description = "Whether to enable point-in-time recovery"
   type        = bool
-  default     = true
 }
 
 variable "attributes" {
   description = "List of nested attribute definitions. Only required for hash_key and range_key attributes. Each attribute has two properties: name - (Required) The name of the attribute, type - (Required) Attribute type, which must be a scalar type: S, N, or B for (S)tring, (N)umber or (B)inary data"
   type        = list(map(string))
-}*/
+}
+
+variable "local_secondary_indexes" {
+  description = "Describe an LSI on the table; these can only be allocated at creation so you cannot change this definition after you have created the resource."
+  type        = any
+}
 
 variable "global_secondary_indexes" {
   description = "Describe a GSI for the table; subject to the normal limits on the number of GSIs, projected attributes, etc."
   type        = any
-  default     = []
-}
-variable "local_secondary_indexes" {
-  description = "Describe an LSI on the table; these can only be allocated at creation so you cannot change this definition after you have created the resource."
-  type        = any
-  default     = []
 }
 
 variable "replica_regions" {
   description = "Region names for creating replicas for a global DynamoDB table."
   type        = any
-  default     = []
 }
 
+variable "server_side_encryption_enabled" {
+  description = "Whether or not to enable encryption at rest using an AWS managed KMS customer master key (CMK)"
+  type        = bool
+}
 
+variable "server_side_encryption_kms_key_arn" {
+  description = "The ARN of the CMK that should be used for the AWS KMS encryption. This attribute should only be specified if the key is different from the default DynamoDB CMK, alias/aws/dynamodb."
+  type        = string
+}
 variable "role_arn" {
   description = " The ARN of the IAM role"
-  type = string
+  type        = string
 }
